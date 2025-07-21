1ec617a63f7a70d76f6c43ec421edf0161ea5350
MartinBarta <barta-martin@seznam.cz>
2025-07-21 16:35:22 +0200

    update

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ foreach ($skins as $skin) {
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<title>test2</title>
+<title>test3</title>
 <style>
     body {
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
